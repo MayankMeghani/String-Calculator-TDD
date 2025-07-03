f22da42966f5dea22aa3897fa9319b0e4d844af8
MayankMeghani <120728455+MayankMeghani@users.noreply.github.com>
2025-07-03 22:56:40 +0530

    step 1:add failing test for string of one number

--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -4,3 +4,9 @@ test("testing with empty string",()=>{
     const calc = new StringCalculator();
     expect(calc.Add("")).toBe(0);
 });
+
+test("returing self when one number is given",()=>{
+    const calc = new StringCalculator();
+    expect(calc.Add("1")).toBe(1);
+    expect(calc.Add("2")).toBe(2);
+})
