71db5d511785d813456a7dd5cee6bbde83fbb88b
MayankMeghani <120728455+MayankMeghani@users.noreply.github.com>
2025-07-03 23:00:51 +0530

    step 1:passed test for one number

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,6 +1,7 @@
 class StringCalculator{
     Add(numbers){
-        if(numbers=="")return 0;
+        if(numbers==="")return 0;
+        if(numbers.length===1) return Number(numbers);
         return 0;
     }
 }
