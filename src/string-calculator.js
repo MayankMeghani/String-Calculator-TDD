2562260690224c384c62aa85dfa6ddee76fe25d2
MayankMeghani <120728455+MayankMeghani@users.noreply.github.com>
2025-07-03 23:07:06 +0530

    step 1:passed test for returning sum of two numbers

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -2,6 +2,10 @@ class StringCalculator{
     Add(numbers){
         if(numbers==="")return 0;
         if(numbers.length===1) return Number(numbers);
+        const nums= numbers.split(",");
+        if(nums.length===2){
+            return Number(nums[0]) + Number(nums[1]);
+        }
         return 0;
     }
 }
