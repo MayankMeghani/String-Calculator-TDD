4f759279510542805d04c717b8111ca4e6154a9b
MayankMeghani <120728455+MayankMeghani@users.noreply.github.com>
2025-07-03 23:18:06 +0530

    step 2:refactoring Add()

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,7 +1,6 @@
 class StringCalculator{
     Add(numbers){
         if(numbers==="")return 0;
-        if(numbers.length===1) return Number(numbers);
         const nums= numbers.split(",").map(Number);
         let sum = 0;
         nums.forEach(n => {    
