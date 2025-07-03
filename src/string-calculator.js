9728e11e5c931c6d0a56806bd7f7d1442de3a14a
MayankMeghani <120728455+MayankMeghani@users.noreply.github.com>
2025-07-03 23:15:56 +0530

    step 2:passed test for returning sum with any amount of numbers

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -2,11 +2,12 @@ class StringCalculator{
     Add(numbers){
         if(numbers==="")return 0;
         if(numbers.length===1) return Number(numbers);
-        const nums= numbers.split(",");
-        if(nums.length===2){
-            return Number(nums[0]) + Number(nums[1]);
-        }
-        return 0;
+        const nums= numbers.split(",").map(Number);
+        let sum = 0;
+        nums.forEach(n => {    
+            sum += n;
+        });
+        return sum;
     }
 }
 
